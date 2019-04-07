529d20e584a39e3e0ca82fd0d250a1519e7ee29b
Jenna Ransom <jenna.ransom@gmail.com>
2019-04-07 00:12:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,7 +34,7 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "h"
-    deal_card += card_total
+    deal_card 
   elsif user_input == "s"
     return card_total
   else
